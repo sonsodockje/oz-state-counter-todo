0ec93280a34c328a572fce6269bef21c7a612132
sonsodockje <hanaroo0115@naver.com>
2024-02-13 23:57:19 +0900

    🐥 zustand로 객체 다루는 방법

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ function App() {
   const count = useStore((state) => state.count);
   const countUp = useStore((state) => state.up);
   const countDown = useStore((state) => state.down);
+
   const todos = useStore((state) => state.todos);
   const addTodo = useStore((state) => state.addTodo);
+
+  const user = useStore((state) => state.user);
+  console.log(user);
+  const addUserObj = useStore((state) => state.addUser);
 
   const [value, setValue] = useState("");
 
@@ -44,6 +49,20 @@ function App() {
             </div>
           ))}
         </ul>
+      </div>
+      <div>
+        {Object.keys(user).map((key) => (
+          <li key={key}>
+            <strong>{key}:</strong> {user[key]}
+          </li>
+        ))}
+        {/* 값만 */}
+        {Object.values(user).map((value) => (
+          <li>{value}</li>
+        ))}
+        {/* 객체의 키는 고유한 값이 아닌데 자료형의 맵은 일단 키가 고유함을 전재로
+        하는 어쩌구 ,, 객체의 값, 값만 빼올땐 좋겠다. 키가 땡땡인 것의 값만
+        가져오게 어케해. */}
       </div>
     </>
   );
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -5,6 +5,15 @@ export const useStore = create((set) => ({
   up: () => set((state) => ({ count: state.count + 1 })),
   down: () => set((state) => ({ count: state.count - 1 })),
 
+  user: {
+    userId: "testididdid",
+    userName: "홍길동",
+  },
+  addUser: ({ userId, userName }) =>
+    set((state) => ({
+      user: { ...state.user, userId, userName },
+    })),
+
   todos: [],
   addTodo: (test) => set((state) => ({ todos: [...state.todos, test] })),
 }));
